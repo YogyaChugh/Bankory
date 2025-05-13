6d2d0a5d69c2a909713166138d8d5bfb6c395dc8
Yogya Chugh <yogyachugh.coder@gmail.com>
2025-05-13 17:32:37 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,16 +7,7 @@ from download_maps import download_map
 
 def main(page: ft.Page):
     os.environ["DRIVE_MAP_ROOT_LINK"] = "Bankory"
-    async def download_the_map(e):
-        down = ft.Text("Downloading")
-        page.add(
-            down
-        )
-        page.update()
-        await download_map('map_01')
-    page.add(
-        ft.FilledButton("Download Map",on_click=download_the_map)
-    )
+    download_map('map_01')
     page.update()
 
 
